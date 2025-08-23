I updated the actual project fixing bugs and I implemented a hubber gradient and tried to fix the plotting error when it was catching error metric instead of gget gradient metric

adam_correction.py
@@ -2,18 +2,24 @@ import numpy as np
 import copy, math
 import matplotlib.pyplot as plt
 
-def compute_cost(X,y,w,b):
+def compute_huber_cost(X,y,w,b,delta = 1.0):
     
-    m = X.shape[0]
-    
-    prediction = np.dot(X, w) + b      #matrix multiplication
+	m = len(X)
+	y_pred = X.dot(w) + b
+	error = y - y_pred
 
-    cost =  (1/(2*m))*np.sum(prediction-y)**2
-    
-    return cost
+	cost = 0.0
+
+	for e in error:
+		if abs(e) <= delta:
+			cost += 0.5*(e**2)
+		else:
+			cost += delta * (abs(e) - 0.5*delta)
+	
+	return cost/m
 
 #This function return dj_db and dj_dw
-def compute_gradient_function_huber_loss(X, y, w, b, delta=1.5):
+def compute_gradient_function_huber_loss(X, y, w, b, delta=1.0):
 
 	m = len(y)
 	y_pred = X.dot(w) + b
@@ -23,12 +29,12 @@ def compute_gradient_function_huber_loss(X, y, w, b, delta=1.5):
 
 	grad = np.where(abs_error <= delta, -error, -delta * np.sign(error))
 
-	dj_dw = (1 / m) * np.dot(X.T, error)
-	dj_db = (1 / m) * np.sum(error)
+	dj_dw = (1 / m) * np.dot(X.T, grad)
+	dj_db = (1 / m) * np.sum(grad)
 
 	return dj_dw, dj_db
 
-def compute_adam(X,y,w,b,learn_rate,num_iter):
+def compute_adam(X,y,w,b,learn_rate,num_iter,delta=1.0):
 	#initializations
 	m_w,m_b = 0, 0
 	v_w,v_b = 0, 0
@@ -38,16 +44,15 @@ def compute_adam(X,y,w,b,learn_rate,num_iter):
 	epsilon = 1e-8		#Adjust to avoid zero division
 	Beta1 = 0.9			#Smooths the gradients
 	Beta2 = 0.999		#Smooths the quadratic gradient
-	
+
 	for i in range(num_iter):
 		step += 1
 		
 		#Getting dw,db and making a cost history
-		dw , db = compute_gradient_function_huber_loss(X,y,w,b)
-		cost = compute_cost(X,y,w,b)
+		dw , db = compute_gradient_function_huber_loss(X,y,w,b,delta)
+		cost = compute_huber_cost(X,y,w,b,delta)
 		
-		if i<100000:      # prevent resource exhaustion 
-			cost_history.append(cost)
+		cost_history.append(cost)
 				
 		#Exponential moment atualizations
 		m_w = Beta1*m_w + (1-Beta1)*dw

compute_gradient.py
@@ -2,18 +2,24 @@ import numpy as np
 import copy, math
 import matplotlib.pyplot as plt
 
-def compute_cost(X,y,w,b):
+def compute_huber_cost(X,y,w,b,delta = 1.0):
     
-    m = X.shape[0]
-    
-    prediction = np.dot(X, w) + b      #matrix multiplication
+	m = len(X)
+	y_pred = X.dot(w) + b
+	error = y - y_pred
 
-    cost =  (1/(2*m))*np.sum(prediction-y)**2
-    
-    return cost
+	cost = 0.0
+
+	for e in error:
+		if abs(e) <= delta:
+			cost += 0.5*(e**2)
+		else:
+			cost += delta * (abs(e) - 0.5*delta)
+	
+	return cost/m
 
 #This function return dj_db and dj_dw with penalities to high loss
-def compute_huber_loss(X,y,w,b,delta = 1.5):
+def compute_huber_gradient(X,y,w,b,delta = 1.0):
     m = len(X)
     y_pred = X.dot(w) + b
 
@@ -34,27 +40,26 @@ def compute_huber_loss(X,y,w,b,delta = 1.5):
         dj_dw += grad*X[i]
         dj_db += grad
 
-        dj_dw = dj_dw/m
-        dj_db = dj_db/m
+    dj_dw = dj_dw/m
+    dj_db = dj_db/m
 
-        return dj_dw,dj_db
+    return dj_dw,dj_db
     
 
-def compute_gradient_descedent(X,y,w,b,alpha,num_iterat,delta = 1.5):
+def compute_gradient_descedent(X,y,w,b,alpha,num_iterat,delta = 1.0):
 
     w = w.copy()        #avoid modifying global w within function
-    j_history = []          #An array that store cost J and w at each iterations       
+    j_history = []      #An array that store cost J and w at each iterations       
 
     for i in range(num_iterat):
-        dj_dw,dj_db =  compute_huber_loss(X,y,w,b,delta)
+        dj_dw,dj_db =  compute_huber_gradient(X,y,w,b,delta)
 
         #Update parameters using w,b,alpha and gradient
         w = w - alpha*dj_dw
         b = b - alpha*dj_db
 
          # Save cost J at each iteration
-        if i<100000:      # prevent resource exhaustion 
-            j_history.append( compute_cost(X, y, w , b))
+        j_history.append(compute_huber_cost(X, y, w , b,delta))
 
         if i % 100 == 0 or i == num_iterat-1:
             print(f"Iteration {i:4}: Cost {j_history[-1]:0.2e}")

compute_regression.py
@@ -29,31 +29,32 @@ try:
   y_train_norm = zscore_normalize_feature(y_train.reshape(-1,1)).flatten()    #first we transforme a vector 1D(n,) in 2D(n,1) after we reshape to 1D
 
   # Initialize parameters
-  alpha = 0.005        #learning_rate
-  num_iter = 10000
-  b = 0
+  alpha = 0.003        #learning_rate
+  num_iter = 5000
+  b = 0           #initial bias
   w = np.zeros(x_train_norm.shape[1])  # initialize the weights for all features
+  delta = 1.0     #turning point between quadratic and linear fuction
 
   # Run gradient descent
   print("Starting gradient descent...")
   w_descedent, b_descendent, j_descedent = compute_gradient_descedent(
-        x_train_norm, y_train_norm, w, b, alpha, num_iter
+        x_train_norm, y_train_norm, w, b, alpha, num_iter,delta
     )
 
   # Run the adam method
   print("Starting adam...")
   w_adam, b_adam, j_adam = compute_adam(
-    x_train_norm,y_train_norm,w,b,alpha,num_iter
+    x_train_norm,y_train_norm,w,b,alpha,num_iter,delta
     )
 
   # Prediction with gradient descendent
   y_pred = x_train_norm @ w_descedent + b_descendent
 
   #Plotting the original data and regression line
-  plt.figure(figsize=(12, 5))
+  plt.figure(figsize=(30, 10))
   plt.subplot(1, 2, 1)
-  plt.scatter(x_train_norm[:, 0], y_train_norm, marker='x', c='r', label="Actual Value")
-  plt.scatter(x_train_norm[:, 0], y_pred, color='blue', label="Predicted Value", alpha=0.5)
+  plt.scatter(x_train_norm[:, 0], y_pred, color='blue', label="Predicted Value", s = 20,zorder = 2)
+  plt.scatter(x_train_norm[:, 0], y_train_norm, marker='x', c='r', label="Actual Value",s = 30,zorder = 3)
   plt.title(f"Feature vs price (first feature: {feature_names[0]})")
   plt.xlabel("Normalized " + feature_names[0])
   plt.ylabel("price")